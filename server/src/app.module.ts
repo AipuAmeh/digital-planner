a30e5c6656f300c49699267478c61e9d1c129213
AipuAmeh <aipuameh.16@gmail.com>
2024-01-23 20:26:43 -0500

    defined todo entity for database

---
+++ server/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      entities: [],
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    TodoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ server/src/todo/entities/todo.entity.ts
@@ -0,0 +1,22 @@
+import { Column } from 'typeorm';
+import { Entity } from 'typeorm';
+import { PrimaryGeneratedColumn } from 'typeorm';
+import { CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  /**
+   * this decorator will help to auto generate id for the table.
+   */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  todo: string;
+
+  @Column({ type: 'varchar', length: 250 })
+  reflectionText: string;
+
+  @CreateDateColumn()
+  todaysDate: Date;
+}
