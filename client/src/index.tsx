db3797dd8c1678dfc363e3b8df912478b0b597fb
AipuAmeh <aipuameh.16@gmail.com>
2024-01-30 15:37:50 -0500

    added delete route functionality to remove task/intention, commented out modal functionality

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: 
+    children: [
+      {
+        index: true,
+        element: <App />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider>
-    <App />
+    <RouterProvider router={router} />
   </ChakraProvider>
-);
+)
+
 
